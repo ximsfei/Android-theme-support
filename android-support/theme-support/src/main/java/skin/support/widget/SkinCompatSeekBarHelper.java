95f4226de166d3e04f8588a85c8b9fdec017adb8
pengfengwang <550616352@qq.com>
2017-09-29 20:46:37 +0800

    SeekBar color filter

--- android-support/theme-support/src/main/java/skin/support/widget/SkinCompatSeekBarHelper.java
+++ android-support/theme-support/src/main/java/skin/support/widget/SkinCompatSeekBarHelper.java
@@ -1,6 +1,8 @@
 package skin.support.widget;
 
+import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.SeekBar;
 
@@ -32,9 +34,26 @@ public class SkinCompatSeekBarHelper extends SkinCompatProgressBarHelper {
     @Override
     public void applySkin() {
         super.applySkin();
-        Drawable drawable = mThumbTypedValue.getDrawable();
-        if (drawable != null) {
-            mView.setThumb(drawable);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Drawable drawable = mThumbTypedValue.getDrawable();
+            if (drawable != null) {
+                mView.setThumb(drawable);
+            }
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            if (mThumbTypedValue.isTypeNull() && mThumbTypedValue.isDataInvalid()) {
+                Drawable drawable = mView.getThumb();
+                if (drawable != null) {
+                    int color = SkinCompatThemeUtils.getColorAccent(mView.getContext());
+                    if (color != 0) {
+                        drawable.setColorFilter(color, PorterDuff.Mode.SRC_IN);
+                    }
+                }
+            } else {
+                Drawable drawable = mThumbTypedValue.getDrawable();
+                if (drawable != null) {
+                    mView.setThumb(drawable);
+                }
+            }
         }
     }
 }
