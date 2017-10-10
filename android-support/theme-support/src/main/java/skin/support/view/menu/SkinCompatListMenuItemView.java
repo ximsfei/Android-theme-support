ec07bb9ef57fa9ff590b062ac5a7441feb4da77f
pengfengwang <550616352@qq.com>
2017-10-10 22:09:33 +0800

    toolbar ListMenuItemView background

--- android-support/theme-support/src/main/java/skin/support/view/menu/SkinCompatListMenuItemView.java
+++ android-support/theme-support/src/main/java/skin/support/view/menu/SkinCompatListMenuItemView.java
@@ -1,8 +1,13 @@
 package skin.support.view.menu;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.RippleDrawable;
+import android.graphics.drawable.StateListDrawable;
+import android.os.Build;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.view.menu.ListMenuItemView;
 import android.support.v7.view.menu.MenuItemImpl;
@@ -13,11 +18,11 @@ import java.lang.reflect.Field;
 import skin.support.R;
 import skin.support.content.res.SkinCompatResources;
 import skin.support.widget.SkinCompatDrawableManager;
-import skin.support.widget.SkinCompatSupportable;
+import skin.support.widget.SkinCompatThemeUtils;
 
 import static skin.support.widget.SkinCompatHelper.INVALID_ID;
 
-public class SkinCompatListMenuItemView extends ListMenuItemView implements SkinCompatSupportable {
+public class SkinCompatListMenuItemView extends ListMenuItemView {
     private int mIconResId = INVALID_ID;
 
     public SkinCompatListMenuItemView(Context context, AttributeSet attrs) {
@@ -76,18 +81,32 @@ public class SkinCompatListMenuItemView extends ListMenuItemView implements Skin
     }
 
     private void applyBackgroundResource() {
-        TypedArray a = SkinCompatResources.getInstance().obtainStyledAttributes(getContext(), null,
-                R.styleable.PopupWindow, R.attr.actionOverflowMenuStyle, 0);
-        Drawable drawable = a.getDrawable(R.styleable.PopupWindow_android_popupBackground);
+        int colorBackground = SkinCompatThemeUtils.getThemeAttrColor(getContext(), android.R.attr.colorBackground);
+        TypedArray a = SkinCompatResources.getInstance()
+                .obtainStyledAttributes(getContext(), new int[]{R.attr.selectableItemBackground});
+
+        Drawable drawable = a.getDrawable(0);
         if (drawable != null) {
-            ViewCompat.setBackground(this, a.getDrawable(R.styleable.PopupWindow_android_popupBackground));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && drawable instanceof RippleDrawable) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    ColorDrawable colorDrawable = new ColorDrawable(
+                            SkinCompatThemeUtils.getThemeAttrColor(getContext(), android.R.attr.colorBackground));
+                    ((RippleDrawable) drawable).addLayer(colorDrawable);
+                } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    ColorDrawable colorDrawable = new ColorDrawable(
+                            SkinCompatThemeUtils.getThemeAttrColor(getContext(), android.R.attr.colorBackground));
+                    ColorStateList rippleColorStateList = getResources().getColorStateList(android.R.color.darker_gray);
+                    if (rippleColorStateList != null) {
+                        drawable = new RippleDrawable(rippleColorStateList, colorDrawable, null);
+                    }
+                }
+            } else if (drawable instanceof StateListDrawable && drawable.getCurrent() instanceof ColorDrawable) {
+                ColorDrawable colorDrawable = (ColorDrawable) drawable.getCurrent();
+                colorDrawable.mutate();
+                colorDrawable.setColor(colorBackground);
+            }
+            ViewCompat.setBackground(this, drawable);
         }
         a.recycle();
-    }
-
-    @Override
-    public void applySkin() {
-        applyIconResource();
-        applyBackgroundResource();
     }
 }
